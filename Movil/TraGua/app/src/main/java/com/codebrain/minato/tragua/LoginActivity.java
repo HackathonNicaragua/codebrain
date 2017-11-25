15b9052cbbab77224bc5c7f60b2512707ae21d88
GLeonardoD <33913719+GLeonardoD@users.noreply.github.com>
2017-11-25 16:03:39 -0600

    Creation Login base
    Base login with class java and xml

---
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.codebrain.minato.tragua;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by David on 25/11/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
