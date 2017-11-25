fd2df841186f880b98a66ef09ce482320a3be6c6
Jerson Martínez <jersonmartinezsm@gmail.com>
2017-11-25 11:29:07 -0600

    Written method public on WebService Controller to Login User validate

--- Web/controllers/WebService.php
+++ Web/controllers/WebService.php
@@ -14,6 +14,18 @@
 			echo $ListWebService;
 		}
 
-		
+		public function Login(){
+			$Model 		= new LoadModel("WebServiceModel");
+			$WebService = new WebServiceModel();
+
+			if (isset($_REQUEST['username']) && isset($_REQUEST['password']))
+				if ($WebService->LoginUser($_REQUEST['username'], $_REQUEST['password']))
+					echo 0;
+				else
+					echo 2;		#Error de logueo, credenciales incorrectas
+			else
+				echo 1; 		#Error al enviar los datos
+		}
+
 	}
 ?>
