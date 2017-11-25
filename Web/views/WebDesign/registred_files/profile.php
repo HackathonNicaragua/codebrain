ab8d70ed8df2d2469820bdb922ae9d8695f49284
Modesto Herrera <modesto.herrera.r17@gmail.com>
2017-11-25 15:19:36 -0600

    añadir archivos

---
+++ Web/views/WebDesign/registred_files/profile.php
@@ -0,0 +1,22 @@
+<head>	<link rel="stylesheet" href="../css/normalizer.css">
+	<link rel="stylesheet" href="../css/extra.css">
+</head>
+<body>
+	<main>
+		<section class="navigation">
+			<section class="Principal"></section>
+			<nav>
+				<ul>
+					<li>Opcion Usuario 1</li>
+					<li>Opcion Usuario 2</li>
+					<li>Opcion Usuario 3</li>
+					<li>Opcion Usuario 4</li>
+					<li>Opcion Usuario 5</li>
+				</ul>
+			</nav>
+		</section>
+		<section class="info">	
+		</section>
+	</main>
+	
+</body>
