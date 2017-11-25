c8a9983b09ef0c2b0005a3838ee1eb3b9aad5829
Jerson Martínez <jersonmartinezsm@gmail.com>
2017-11-25 11:26:33 -0600

    Written method public to Clean String

--- Web/models/WebServiceModel.php
+++ Web/models/WebServiceModel.php
@@ -3,5 +3,48 @@
 		public function getIndex(){
 			return "Welcome...";
 		}
+
+		public function CleanString($str) {
+		    $str = trim($str);
+		 	
+		    $str = str_replace(
+		        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+		        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+		        $str
+		    );
+
+		    $str = str_replace(
+		        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+		        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+		        $str
+		    );
+
+		    $str = str_replace(
+		        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+		        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+		        $str
+		    );
+
+		    $str = str_replace(
+		        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+		        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+		        $str
+		    );
+
+		    $str = str_replace(
+		        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+		        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+		        $str
+		    );
+		 
+		    $str = str_replace(
+		        array('ñ', 'Ñ', 'ç', 'Ç'),
+		        array('n', 'N', 'c', 'C',),
+		        $str
+		    );
+
+		    #Se retorna el nuevo string.
+		    return $str;
+		}
 	}
 }
