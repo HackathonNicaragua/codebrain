2676d0cfdc544abb9bb2364baad1987993e3c23d
abield95 <santosabield55@gmail.com>
2017-11-25 13:40:35 -0600

    Using for storing constant values

---
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/ConstantValues.java
@@ -0,0 +1,9 @@
+package com.codebrain.minato.tragua;
+
+/**
+ * Created by username on 11/25/2017.
+ */
+
+public abstract class ConstantValues {
+    static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+}
