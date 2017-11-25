cf33d8771c1b7b8224c64cabf5f641bca3ce1a9a
Jerson Martínez <jersonmartinezsm@gmail.com>
2017-11-25 11:32:14 -0600

    Written method public on WebService Model to Add new user and check self

--- Web/models/WebServiceModel.php
+++ Web/models/WebServiceModel.php
@@ -74,5 +74,36 @@
 
 	    	return false;
 	    }
+
+	    public function AddUser($Array){
+	    	$username 	= empty($Array['username']) 	? "-" : $Array['username'];
+	    	$username 	= $this->CleanString($username);
+
+	    	$password 	= empty($Array['password']) 	? "-" : $Array['password'];
+	    	$firstname 	= empty($Array['firstname']) 	? "-" : $Array['firstname'];
+	    	$lastname 	= empty($Array['lastname']) 	? "-" : $Array['lastname'];
+	    	$email 		= empty($Array['email']) 		? "-" : $Array['email'];
+	    	
+	    	$q 			= "INSERT INTO user(username, password) VALUES (:username,:password);";
+	    	$stmt 		= $this->db->prepare($q);
+
+	    	$stmt->bindValue(":username", $username);
+	    	$stmt->bindValue(":password", password_hash($password, PASSWORD_DEFAULT));
+	    
+	    	if ($stmt->execute())
+	    		if ($this->AddUserInfo($username, $firstname, $lastname, $email))
+	    			return true;
+
+	    	return false;
+	    }
+
+	    public function CheckUser($usr){
+	    	$R = $this->db->query("SELECT * FROM user WHERE username='".$usr."';");
+
+	    	if ($R->rowCount() > 0)
+	    		return false;
+
+	    	return true;
+	    }
 	}
 }
