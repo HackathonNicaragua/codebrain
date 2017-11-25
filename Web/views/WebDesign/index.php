7706d65d71964b6d0dc2e2cbb5fe6b5d70789843
Modesto Herrera <modesto.herrera.r17@gmail.com>
2017-11-25 11:29:55 -0600

    Diseño
    + Se agregaron carpetas css, js, img:
    + Plugins añadidos
        + Jquery
        + modernizr & normalizer
        + Google Maps (css + js)
             + Api & Libraries
        + Plantilla Perspective  (css + js)
    
    + Archivos js y css (Para trabajarlos)

---
+++ Web/views/WebDesign/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Tragua</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="views/WebDesign/css/normalizer.css">
+	<link rel="stylesheet" href="views/WebDesign/css/plantilla.css">
+	<link rel="stylesheet" href="views/WebDesign/css/css.css">
+	<script src="js/jquery.js"></script>
+	<script src="views/WebDesign/js/jquery.js"></script>
+	<script src="views/WebDesign/js/modernizr.js"></script>
+	<script src="views/WebDesign/js/plantilla.js"></script>
+	<script src="views/WebDesign/js/js.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key= AIzaSyD-wrx6MwViWyxBmyIiu07Q8SOrqLszc4o&callback=initMap&libraries=places" async defer></script>
+</head>
+<body>
+	
+</body>
+</html>
---
+++ Web/views/WebDesign/js/js.js
@@ -0,0 +1,145 @@
+$(document).ready(function() {
+	// Perspectivas Plugin1
+	var contentWrapper = $('.wrapper'), perspectiveWrapper = $('#perspective')[0], support = Modernizr.csstransitions, 
+	container = $( '.container' ), transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend','OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' }, 
+	transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
+
+	$('#Outer1, #Outer2').click(function(event) {
+		if ($(this).attr('id') == 'Outer1') {
+			$('#perspective').removeClass('effect-moveleft').addClass('effect-laydown');
+			$('nav.outer-nav').removeClass('right').addClass('hidden');
+			$('.login').addClass('right').removeClass('hidden');
+		}
+		else {
+			$('#perspective').removeClass('effect-laydown').addClass('effect-moveleft');
+			$('.login').removeClass('right').addClass('hidden');
+			$('nav.outer-nav').addClass('right').removeClass('hidden');
+		}
+		docscroll = scrollY();
+		contentWrapper.top = docscroll * -1 + 'px';
+		$('body').scrollTop(0);
+		classie.add( perspectiveWrapper , 'modalview' );
+		setTimeout( function() { classie.add( perspectiveWrapper, 'animate' ); }, 25 );
+	});
+
+	$('.container').click(function(event) {
+		if( classie.has( perspectiveWrapper, 'animate') ) {
+			var onEndTransFn = function( ev ) {
+				if( support && ( ev.target.className !== 'container' || ev.propertyName.indexOf( 'transform' ) == -1 ) ) return;
+				this.removeEventListener( transEndEventName, onEndTransFn );
+				classie.remove( perspectiveWrapper, 'modalview' );
+				$('body').scrollTop(docscroll);
+			};
+			if( support ) {
+				perspectiveWrapper.addEventListener( transEndEventName, onEndTransFn );
+			}
+			else {
+				onEndTransFn.call();
+			}
+			classie.remove( perspectiveWrapper, 'animate' );
+			$('.login, nav.outer-nav').removeClass('right').addClass('hidden');
+		}
+	});
+
+	function scrollY() {
+		return window.pageYOffset || window.scrollTop;
+	}
+
+});
+// Google Maps
+var latlon;
+function initMap() {
+   	var map = new google.maps.Map($('#map').get(0), { center: {lat: 12.437346, lng: -86.87722}, zoom: 16,
+          disableDefaultUI: true });
+   	var infoWindow = new google.maps.InfoWindow( {map: map} );
+	
+    if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+    	   	var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Hola, tu estas aqui! "+
+            	"<br /> Lat: " + pos.lat + 
+            	"<br /> Lon:"  + pos.lng );
+            map.setCenter(pos);
+            latlon = {lat: parseFloat(pos.lat), lng: parseFloat(pos.lng)};
+        }, function() {
+        	handleLocationError(true, infoWindow, map.getCenter());
+        });
+   	} else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    new AutocompleteDirectionsHandler(map);
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+   	infoWindow.setPosition(pos); 
+    infoWindow.setContent(browserHasGeolocation ? 
+    	'Error: Fallo el servicio de Geolocalización' : 'Error: Navegador incompatible.');
+}
+
+// Auto complete
+function AutocompleteDirectionsHandler(map) {
+    this.map = map;
+    this.originPlaceId = null;
+    this.destinationPlaceId = null;
+    this.travelMode = 'WALKING';
+    var originInput = document.getElementById('origin-input');
+    var destinationInput = document.getElementById('destination-input');
+    var modeSelector = document.getElementById('mode-selector');
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setMap(map);
+
+    var originAutocomplete = new google.maps.places.Autocomplete( originInput, {placeIdOnly: true});
+    var destinationAutocomplete = new google.maps.places.Autocomplete( destinationInput, {placeIdOnly: true});
+
+    this.setupClickListener('changemode-walking', 'WALKING');
+    this.setupClickListener('changemode-driving', 'DRIVING');
+
+    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+}
+
+AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+    var radioButton = document.getElementById(id), me = this;
+    radioButton.addEventListener('click', function() { me.travelMode = mode; me.route(); });
+};
+
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.place_id) {
+        	window.alert("Please select an option from the dropdown list.");
+            return;
+        }
+        if (mode === 'ORIG') {
+        	me.originPlaceId = place.place_id;
+        } else {
+        	me.destinationPlaceId = place.place_id;
+        }
+        me.route();
+    });
+};
+
+AutocompleteDirectionsHandler.prototype.route = function() {
+    if (!this.destinationPlaceId) { return; }
+    var me = this;
+	this.directionsService.route({
+	    origin: {'lat': latlon.lat,'lng': latlon.lng },
+	    destination: {'placeId': this.destinationPlaceId},
+	    travelMode: this.travelMode
+	}, function(response, status) {
+	    if (status === 'OK') {
+			me.directionsDisplay.setDirections(response);
+		} else {
+			window.alert('Directions request failed due to ' + status);
+		}
+	});
+};
