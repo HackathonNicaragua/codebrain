99c45f7cb52a0d7e681e5ba926db67b206f6bcce
abield95 <santosabield55@gmail.com>
2017-11-25 14:47:28 -0600

    Used to pass data from the dialog to the activity

---
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/CustomDialogs/DialogListener.java
@@ -0,0 +1,11 @@
+package com.codebrain.minato.tragua.CustomDialogs;
+
+import android.os.Bundle;
+
+/**
+ * Created by username on 11/25/2017.
+ */
+
+public interface DialogListener {
+    void onCompleteDialog(Bundle args);
+}
