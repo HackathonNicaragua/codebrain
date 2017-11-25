ce0cba1474ebb3d6ca6284a33f0c6a09794696bd
Jerson Martínez <jersonmartinezsm@gmail.com>
2017-11-25 10:41:55 -0600

    Ready, Model connections to the DB

---
+++ Web/core/Model.php
@@ -0,0 +1,33 @@
+<?php
+
+class Model {
+	public $db = null;
+
+	function __construct(){
+		try {
+			$this->getConnection();
+		} catch (PDOException $e) {
+		    die("Falló la conexión: ".$e->getMessage());
+		}
+	}
+
+	public function getConnection(){
+		$host = "127.0.0.1";
+        $user = "SideMaster";
+        $pass = "Inform@tico";
+        $dbase = "codebrain";
+
+		$cn = sprintf("host=%s;dbname=%s", $host, $dbase);
+
+		if ($this->db = new PDO("mysql:".$cn, $user, $pass)){
+			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+			if (@$this->db->query("SET NAMES 'utf8'"))
+				return true;
+		}
+
+		return false;
+	}
+}
+
+?>
