4a8da0372212369586d4928065a433b2aa46eba0
GLeonardoD <33913719+GLeonardoD@users.noreply.github.com>
2017-11-25 17:30:20 -0600

    Activity Login and Registry finish and funcionation correct!

--- Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/LoginActivity.java
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.codebrain.minato.tragua;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by David on 25/11/2017.
@@ -14,5 +17,13 @@ public class LoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button button = findViewById(R.id.login_boton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),RegistryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
