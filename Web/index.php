52e8e3d4fa530ece68316715c8fc3c4cbb128089
Jerson Martínez <jersonmartinezsm@gmail.com>
2017-11-25 10:47:28 -0600

    MVC correct

--- Web/index.php
+++ Web/index.php
@@ -5,5 +5,13 @@
 	include ("./core/Model.php");
 	include ("./core/LoadModel.php");
 
-	
+	if ($_REQUEST && isset($_REQUEST['controller']))
+		$default_controller = $_REQUEST['controller'];
+
+	if (file_exists("controllers/".$default_controller.".php"))
+		include ("controllers/".$default_controller.".php");
+	else
+		die("Controlador no encontrado!");
+
+	(new $default_controller());
 ?>
