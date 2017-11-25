36cd25945c9059db0b65ff05228107c0ae044652
Jerson Martínez <jersonmartinezsm@gmail.com>
2017-11-25 10:47:11 -0600

    Making test on index.php

---
+++ Web/index.php
@@ -0,0 +1,9 @@
+<?php
+	include ("./core/Controller.php");
+	include ("./core/Config.php");
+	include ("./core/View.php");
+	include ("./core/Model.php");
+	include ("./core/LoadModel.php");
+
+	
+?>
