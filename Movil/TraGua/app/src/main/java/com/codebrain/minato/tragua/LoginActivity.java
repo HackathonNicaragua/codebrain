6a7eb174122d660e11e0cb7b93873988e74330fd
abield95 <santosabield55@gmail.com>
2017-11-25 18:20:52 -0600

    Se agrego funcion para validar que los campos cumplan los requerimientos

--- Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/LoginActivity.java
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/LoginActivity.java
@@ -4,27 +4,77 @@ package com.codebrain.minato.tragua;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by David on 25/11/2017.
  */
 
 public class LoginActivity extends NavigationDrawerBaseActivity{
-    
+    private EditText username, password;
+    private AppCompatButton btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button button = findViewById(R.id.login_button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        username = (EditText)findViewById(R.id.login_username);
+        password = (EditText)findViewById(R.id.login_password);
+        btLogin = (AppCompatButton)findViewById(R.id.login_button);
+        btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),RegistryActivity.class);
-                startActivity(intent);
+                userLogin();
             }
         });
+
+    }
+
+    private void userLogin()
+    {
+        if (!validate())
+        {
+            onLoginFailed();
+            return;
+        }
+    }
+
+    private boolean validate()
+    {
+        boolean valid = true;
+
+        String username = this.username.getText().toString();
+        String password = this.password.getText().toString();
+
+        if (username.isEmpty())
+        {
+            this.username.setError(getResources().getString(R.string.empty_username));
+            valid = false;
+        }
+        else
+        {
+            this.username.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 20)
+        {
+            this.password.setError(getResources().getString(R.string.password_incorrect));
+            valid = false;
+        }
+        else
+        {
+            this.password.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void onLoginFailed()
+    {
+
     }
 }
