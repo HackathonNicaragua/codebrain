7167b7cd5f969fbcce14b431b3d7954f486440ce
abield95 <santosabield55@gmail.com>
2017-11-26 04:47:50 -0600

    Creacion de actividad e inclusione en el manifiesto

---
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/BusinessHomaPageActivity.java
@@ -0,0 +1,18 @@
+package com.codebrain.minato.tragua;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by username on 11/26/2017.
+ */
+
+public class BusinessHomaPageActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.business_home_page_layou);
+    }
+}
