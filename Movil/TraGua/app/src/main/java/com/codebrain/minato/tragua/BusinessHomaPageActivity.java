2750bb0e3565b28993321fd3d6580e8310576ea7
abield95 <santosabield55@gmail.com>
2017-11-26 08:44:38 -0600

    Some updates

--- Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/BusinessHomaPageActivity.java
+++ Movil/TraGua/app/src/main/java/com/codebrain/minato/tragua/BusinessHomaPageActivity.java
@@ -1,18 +1,74 @@
 package com.codebrain.minato.tragua;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.codebrain.minato.tragua.CustomDialogs.DialogListener;
+import com.codebrain.minato.tragua.CustomDialogs.RateDialog;
 
 /**
  * Created by username on 11/26/2017.
  */
 
-public class BusinessHomaPageActivity extends AppCompatActivity {
+public class BusinessHomaPageActivity extends AppCompatActivity implements DialogListener{
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.business_home_page_layou);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_business);
+        LinearLayout valorar = (LinearLayout)findViewById(R.id.business_valorar_bt);
+        valorar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                RateDialog dialog = new RateDialog();
+                dialog.show(fragmentManager, "rating");
+            }
+        });
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        setActionBar(toolbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+
     }
+
+    @Override
+    public void onCompleteDialog(Bundle bundle)
+    {
+        switch (bundle.getInt("dialog"))
+        {
+            case 3:
+                Log.d("Manejado", "Respuesta de RaeDialog");
+                //rate dialog
+                Toast.makeText(getApplicationContext(), "rating" + bundle.getFloat("rating"), Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item)
+//    {
+//        switch (item.getItemId())
+//        {
+//            case R.id.homeAsUp:
+//                finish();
+//                break;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
